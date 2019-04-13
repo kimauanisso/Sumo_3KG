ba805a9da9aaa78f25a3c3fa6c1e88ed79c21ba5
andersonhm <anderson.hmoreira@gmail.com>
2019-04-13 19:18:46 -0300

    RSM Challenge 2019
    Rotina de acionamento dos motores
    
    Implementado Funcionamento Básico
    
    Programa com rotinas de busca para o RSM Challenge
    
    Versão depois do RSM Challenge

---
+++ Sumo_3KG/src/define.h
@@ -0,0 +1,63 @@
+#ifndef define
+#define define
+
+#include <Arduino.h>
+#include <Servo.h>
+
+/************Definição de pinos*************/
+//Receptor
+#define PIN_CH1 A0
+#define PIN_CH2 A1
+#define PIN_CH3 A2
+//Sensores de Presença
+#define PIN_SP_F 9
+#define PIN_SP_FDIR 7
+#define PIN_SP_FESQ 11
+#define PIN_SP_DDIR 8
+#define PIN_SP_DESQ 10
+#define PIN_SP_LDIR 6
+#define PIN_SP_LESQ 12
+//DIP Switch
+#define PIN_DIP1 4
+#define PIN_DIP2 3
+#define PIN_DIP3 2
+//Sensores de Linha
+#define PIN_SL_DIR A5
+#define PIN_SL_ESQ A6
+//Motores
+#define PIN_MOT_DIR A4
+#define PIN_MOT_ESQ A3
+
+//Definição de variáveis da leitura do receptor
+//deltaT -> Tamanho do pulso do sinal de Servo do receptor
+//tZero_CH1 -> Guarda o tempo inicial de leitura do sinal do receptor
+volatile uint16_t deltaT_CH1, deltaT_CH2, deltaT_CH3;
+static uint16_t deltaT_CH1_Temp, deltaT_CH2_Temp, deltaT_CH3_Temp;
+uint16_t tZero_CH1, tZero_CH2, tZero_CH3;
+
+//Variáveis dos motores
+Servo motorDir;
+Servo motorEsq;
+
+//Variáveis de sensores e outros inputs
+uint8_t valorDIP;         //variáveis dos estados do DIP Switch
+uint8_t sensoresPresenca; //variável que armazena o estado de todos os sensores de presença
+
+//Variável para avanço full
+int flagAvanco = 0;
+
+//Protótipos de funções
+void lerSensores();
+void lerDIP();
+void lerReceptor();
+void buscaSimples(int, int, int, int);
+void moveRobo(int, int);
+void exibeSensores();
+void exibeDIP();
+void exibeReceptor(uint16_t, uint16_t, uint16_t);
+void controlaRobo(uint16_t, uint16_t);
+void frente(int, int, int);
+void cedilha(int, int, int, char);
+void gira180(int, int, int);
+
+#endif
--- Sumo_3KG/src/main.cpp
+++ Sumo_3KG/src/main.cpp
@@ -1,9 +1,88 @@
-#include <Arduino.h>
+#include "rotinasRobo.h"
 
-void setup() {
-  // put your setup code here, to run once:
+void setup()
+{
+  Serial.begin(9600);
+  //Configuração dos Motores
+  motorDir.attach(PIN_MOT_DIR, 1000, 2000);
+  motorEsq.attach(PIN_MOT_ESQ, 1000, 2000);
+  // motorDir.writeMicroseconds(1500);
+  // motorEsq.writeMicroseconds(1500);
+  // delay(200);
+
+  //Configuração dos Sensores
+  pinMode(PIN_SP_LDIR, INPUT_PULLUP);
+  pinMode(PIN_SP_DDIR, INPUT_PULLUP);
+  pinMode(PIN_SP_FDIR, INPUT_PULLUP);
+  pinMode(PIN_SP_F, INPUT_PULLUP);
+  pinMode(PIN_SP_FESQ, INPUT_PULLUP);
+  pinMode(PIN_SP_DESQ, INPUT_PULLUP);
+  pinMode(PIN_SP_LESQ, INPUT_PULLUP);
+  //Configuração DIP Switch
+  pinMode(PIN_DIP1, INPUT_PULLUP);
+  pinMode(PIN_DIP2, INPUT_PULLUP);
+  pinMode(PIN_DIP3, INPUT_PULLUP);
+  //Configuração interrupts do receptor
+  attachInterrupt(PIN_CH1, lerCH1, CHANGE);
+  attachInterrupt(PIN_CH2, lerCH2, CHANGE);
+  attachInterrupt(PIN_CH3, lerCH3, CHANGE);
+  delay(2000);
+  lerReceptor();
+  controlaRobo(deltaT_CH1_Temp, deltaT_CH2_Temp);
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  lerReceptor();
+  if (deltaT_CH3_Temp < 1900  && deltaT_CH3_Temp > 1100)
+  {
+    controlaRobo(deltaT_CH1_Temp, deltaT_CH2_Temp);
+    //exibeReceptor(deltaT_CH1_Temp, deltaT_CH2_Temp, deltaT_CH3_Temp);
+  }
+  else
+  {
+    lerDIP();
+    delay(4800);
+    //exibeDIP();
+    switch (valorDIP)
+    {
+    case 0b11111111:
+      //Serial.println("Parado");
+      moveRobo(0, 0);
+      break;
+    case 0b11111110:
+      //Serial.println("Frente");
+      frente(100, 65, 140);
+      break;
+    case 0b11111101:
+      if (deltaT_CH3_Temp > 1800)
+      {
+        //Serial.println("Cedilha Esq");
+        cedilha(100, 40, 235, 'D');
+      }
+      else
+      {
+        //Serial.println("Cedilha Dir");
+        cedilha(100, 65, 180, 'e');
+      }
+      break;
+    case 0b11111100:
+      //Serial.println("Gira 180");
+      gira180(100, -100, 120);
+      break;
+    default:
+      //Serial.println("Erro");
+      break;
+    }
+
+    while (deltaT_CH3_Temp > 1800 || deltaT_CH3_Temp < 1200)
+    {
+      lerReceptor();
+      lerSensores();
+      buscaSimples(100, 70, 50, 30); //(0, 70, 50, 30);
+      //exibeSensores();
+    }
+  }
 }
